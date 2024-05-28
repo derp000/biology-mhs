e68cb5f39cd7c902406680b3715c60bd35fe9bb0
derp000 <84947701+derp000@users.noreply.github.com>
2024-05-28 08:35:14 -0400

    Add to Chapter1Questions.ts

--- frontend/src/questions/Chapter1Questions.ts
+++ frontend/src/questions/Chapter1Questions.ts
@@ -21,6 +21,198 @@ const questions: QuizQuestionList = [
     correctIndex: 1,
     chapter: CH_NUM,
   },
+  {
+    question: "The smallest living unit is a _____.",
+    options: ["cell", "atom", "molecule", "tissue"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question: "Similar cells with a similar function describes a _____.",
+    options: ["organ", "population", "tissue", "organ system"],
+    correctIndex: 2,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "All of the gray squirrels in a temperate deciduous forest are referred to as a _____.",
+    options: ["community", "population", "ecosystem"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question: "Unicellular organisms cannot respond to their environment.",
+    options: ["True", "False"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "The ultimate source of energy for nearly all living things is _____.",
+    options: ["fossil fuels", "the sun", "thermal vents", "water"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question: "Homeostasis is _____.",
+    options: [
+      "balancing internal and external conditions",
+      "wild fluctuations in internal conditions",
+      "the transformation of the sun's energy into organic molecules",
+      "the maintenance of internal conditions within certain limits",
+    ],
+    correctIndex: 3,
+    chapter: CH_NUM,
+  },
+  {
+    question: "The ability to respond _____.",
+    options: [
+      "ensures survival of the organism",
+      "allows the organism to carry on its daily activities",
+      "all of the above",
+    ],
+    correctIndex: 2,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "Only plants are able to capture solar energy and carry on photosynthesis.",
+    options: ["True", "False"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question: "All cells have a copy of the hereditary material, DNA.",
+    options: ["True", "False"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "The instructions for the organization and metabolism of living things are encoded in the _____.",
+    options: ["genes", "plasma membrane", "cell wall", "ribosomes"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "Some organisms reproduce by simply splitting in two in a process called _____.",
+    options: ["mitosis", "meiosis", "vegetative propogation", "binary fission"],
+    correctIndex: 3,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "In most multicellular organisms, reproduction involves the pairing of a sperm from one partner and an egg from the other partner.",
+    options: ["True", "False"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "The characteristics of living things suggests that living things descended from several different ancestors.",
+    options: ["True", "False"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question: "Hypotheses can be tested in _____.",
+    options: [
+      "laboratory experiments",
+      "field experiments",
+      "all of the above",
+    ],
+    correctIndex: 2,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "When a person uses isolated facts and creative thinking to arrive at an explanation for something, he/she used _____ reasoning.",
+    options: ["inductive", "deductive"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "Deductive reasoning begins with a general statement that implies a specific conclusion.",
+    options: ["True", "False"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
+  {
+    question: "A control group experiences all the steps in an experiment.",
+    options: ["True", "False"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "The component in an experiment being tested is called the _____ variable.",
+    options: ["control", "experimental", "dependent"],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "An experiment is done to see if eating breakfast affects students' performance on exams. The experimental variable is _____.",
+    options: [
+      "the students' scores on the exams",
+      "the age of the student",
+      "whether the student has breakfast or not",
+      "whether the student drinks coffee or not",
+    ],
+    correctIndex: 2,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "Which of the following is an assumption scientists must accept when performing the scientific process?",
+    options: [
+      "natural laws are not affected by time",
+      "nature is real, understandable, and knowable by observation",
+      "nature is orderly and uniform",
+      "all of the above",
+    ],
+    correctIndex: 3,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "Theories that are generally accepted by an overwhelming number of scientists are called _____.",
+    options: ["principles", "laws", "both"],
+    correctIndex: 2,
+    chapter: CH_NUM,
+  },
+  {
+    question: "The first step in the scientific method is _____.",
+    options: [
+      "make conclusions",
+      "make observations and consider previous data",
+      "formulate a hypothesis",
+      "test a related hypothesis",
+    ],
+    correctIndex: 1,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "If an experiment is done to see if a dietary supplement affects the weight gain of chickens, what variables need to be controlled?",
+    options: [
+      "where the chickens are kept",
+      "the availability of water",
+      "the temperature of the surroundings",
+      "all of the above",
+    ],
+    correctIndex: 3,
+    chapter: CH_NUM,
+  },
+  {
+    question:
+      "If the research subjects are different, it is hoped that random selection distributes them equally among the groups.",
+    options: ["True", "False"],
+    correctIndex: 0,
+    chapter: CH_NUM,
+  },
 ];
 
 export default questions;
